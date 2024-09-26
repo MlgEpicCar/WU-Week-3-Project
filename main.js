fd1209b69b4ca9d1ce61d9dc559f4a70d9ebf0c1
MlgEpicCar <115513939+MlgEpicCar@users.noreply.github.com>
2024-09-26 14:28:34 -0400

    a

--- main.js
+++ main.js
@@ -17,12 +17,12 @@ for (let i = 0; i < 5; i++) {
     document.getElementById("divFear").appendChild(li);
 }
 
-var img1 = document.createElement("img");
+img1 = document.createElement("img");
 img1.src = "/images/giraffe.jpg";
-var src = document.getElementById("img1");
+src = document.getElementById("img1");
 src.appendChild(img1);
 
-var img2 = document.createElement("img");
+img2 = document.createElement("img");
 img2.src = "/images/rhino.jpg";
-var src = document.getElementById("img2");
+src = document.getElementById("img2");
 src.appendChild(img2);
