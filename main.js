99d5016e61ab8596426e5427a6da58e0136701ed
MlgEpicCar <115513939+MlgEpicCar@users.noreply.github.com>
2024-09-26 13:53:38 -0400

    shifted the task of creation onto javascript

--- main.js
+++ main.js
@@ -1,22 +1,24 @@
 const divFavs = ["Giraffes", "Dogs", "Horses", "Cats", "Birds"];
 const divFear = ["Rhinoceros", "Tiger", "Spider", "Lion", "Shark"];
 
-document.getElementById("h1").textContent = `Top Favorite Animals`;
+ul = document.createElement('ul');
+ul.setAttribute("id", "divFavs");
 
-document.getElementById("li0").textContent = divFavs[0];
-document.getElementById("li1").textContent = divFavs[1];
-document.getElementById("li2").textContent = divFavs[2];
-document.getElementById("li3").textContent = divFavs[3];
-document.getElementById("li4").textContent = divFavs[4];
+document.getElementById("h1").textContent = `Top Favorite Animals`; // h1
 
+for (let i = 0; i < 5; i++) {
+    li = document.createElement("li"); // creates list item
+    li.innerText = divFavs[i]; // puts String from array into list item
+    document.getElementById("divFavs").appendChild(li); // appends list item
+}
 
 document.getElementById("h2").textContent = `Top Feared Animals`;
 
-document.getElementById("li5").textContent = divFear[0];
-document.getElementById("li6").textContent = divFear[1];
-document.getElementById("li7").textContent = divFear[2];
-document.getElementById("li8").textContent = divFear[3];
-document.getElementById("li9").textContent = divFear[4];
+for (let i = 0; i < 5; i++) {
+    li = document.createElement("li");
+    li.innerText = divFear[i];
+    document.getElementById("divFear").appendChild(li);
+}
 
 var img1 = document.createElement("img");
 img1.src = "/images/giraffe.jpg";
